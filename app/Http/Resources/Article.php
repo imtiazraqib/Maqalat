72d9998b0975b10cc20d9dc75dd48ec1e6ccf295
imtiaz-raqib <imtiazud@ualberta.ca>
2020-01-07 11:32:33 -0700

    Customize resource to remove created and updated keys

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -14,6 +14,13 @@ class Article extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        // Get rid of 'created at' and 'updated at' keys from the data
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body
+        ];
     }
 }
